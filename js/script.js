a8d5b115e8f075d7f1f4ad6ac39b081b25d946f4
Daiki-Toyoda <63102490+Daiki-Toyoda@users.noreply.github.com>
2022-04-06 01:47:17 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -54,4 +54,15 @@ $(function() {
       $("#contents").css("margin-top", "0");
     }
   });
+
+  $(window).scroll(function () {
+    const wHeight = $(window).height();
+    const scrollAmount = $(window).scrollTop();
+    $('.scrollanime').each(function () {
+        const targetPosition = $(this).offset().top;
+        if(scrollAmount > targetPosition - wHeight + 60) {
+            $(this).addClass("fadeInDown");
+        }
+    });
+});
 });
